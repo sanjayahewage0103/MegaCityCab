83de2d5547ff2aad367112ff658c49b4b0260fd7
Sanjaya Hewage <pethumhewage66@gmail.com>
2025-03-05 03:53:21 +0530

    create AdminService for business logic

---
+++ src/main/java/com/example/megacitycab/services/AdminService.java
@@ -0,0 +1,16 @@
+package com.example.megacitycab.services;
+
+import com.example.megacitycab.util.AdminCredentialsManager;
+
+public class AdminService {
+    private AdminCredentialsManager adminCredentialsManager;
+
+    public AdminService() {
+        this.adminCredentialsManager = AdminCredentialsManager.getInstance();
+    }
+
+    // Method to authenticate admin
+    public boolean authenticateAdmin(String username, String password) {
+        return adminCredentialsManager.validateAdmin(username, password);
+    }
+}
